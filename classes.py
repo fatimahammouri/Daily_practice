dc05ab67e8c24a5efb56a540e1e3e210cfa833e2
Fatima <fatima.hammouri@gmail.com>
2021-07-07 20:26:26 +0000

    added gcd function to find common divisor

--- classes.py
+++ classes.py
@@ -20,6 +20,19 @@ class Fraction:
         result_num = self.num * other_fraction.den + other_fraction.num * self.den
         result_den = self.den * other_fraction.den
         return Fraction(result_num, result_den)
+
+def gcd (m, n):
+    """ Helper function to find greatest common divisor 
+    to find the lowest term representation of a fraction"""
+
+    while m % n != 0:
+        orig_m = m
+        orig_n = n
+
+        m = orig_n
+        n = orig_m % orig_n
+        
+    return n
     
 my_fraction = Fraction(5,10)
 my_fraction.show()
