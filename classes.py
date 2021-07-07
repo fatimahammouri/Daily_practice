b96b5dd0765200f44c56c22cf8afd6756357d9a0
Fatima <fatima.hammouri@gmail.com>
2021-07-07 17:06:44 +0000

    added show method to class fraction

--- classes.py
+++ classes.py
@@ -9,4 +9,10 @@ class Fraction:
         self.num = num
         self.den = den
 
+    def show(self):
+        """ method to show the Fraction object """
+        
+        print(self.num, "/", self.den)
     
+my_fraction = Fraction(5,10)
+my_fraction.show()
