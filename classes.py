06f461fadf5aa9a3ab05edd81a4959fdd6f2c0c0
Fatima <fatima.hammouri@gmail.com>
2021-07-07 19:52:37 +0000

    added add method to class fraction

--- classes.py
+++ classes.py
@@ -13,6 +13,13 @@ class Fraction:
         """ method to show the Fraction object """
         
         print(self.num, "/", self.den)
+
+    def add(self, other_fraction):
+        """method adds two fractions, return result fraction"""
+
+        result_num = self.num * other_fraction.den + other_fraction.num * self.den
+        result_den = self.den * other_fraction.den
+        return Fraction(result_num, result_den)
     
 my_fraction = Fraction(5,10)
 my_fraction.show()
