a5834fc23668d2d8920fc1864996b6c473a29142
Fatima <fatima.hammouri@gmail.com>
2021-07-07 17:03:28 +0000

    practice1: create a Fraction Class

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Fraction:
+    """ implementing a fraction class 
+        attributes:
+        - numerator: any integer
+        - denominator: any integer > 0
+    """
+
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+
+    
