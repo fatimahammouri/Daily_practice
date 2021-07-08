0f540e11e550f7483b8cbe8558ebfd1691b60bd1
Fatima <fatima.hammouri@gmail.com>
2021-07-08 07:34:18 +0000

    1st challenge

---
+++ challenges.py
@@ -0,0 +1,26 @@
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?
+
+    >>> is_anagram_of_palindrome("a")
+    True
+    >>> is_anagram_of_palindrome("ab")
+    False
+    >>> is_anagram_of_palindrome("aab")
+    True
+    >>> is_anagram_of_palindrome("arceace")
+    True
+    >>> is_anagram_of_palindrome("arceaceb")
+    False
+    """
+
+    # create a dictionary where 
+    # keys: character in given word
+    # values: occurences of each character
+    # if there is more than one char whos value == 1
+    # return false
+
+    
+
+
+
+             
