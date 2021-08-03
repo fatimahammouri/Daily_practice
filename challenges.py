ded805361bbfcc827d0b6355887d965cdf681995
Fatima <fatima.hammouri@gmail.com>
2021-08-03 10:26:04 +0000

    JS Challenge #1

--- challenges.py
+++ challenges.py
@@ -247,3 +247,47 @@ def solution(Arr):
             start += 1
         else:
             return start
+"""A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps."""
+
+
+def solution(N):
+    # find the binary representation of the number
+    # declare a counter  
+    # loop over the 0s and 1s 
+    # check if char is 1 
+    # check if 1 / 0 comes after it
+    # if 1 == > continue 
+    #  if 0 continue counter + 1
+
+    binN = list(bin(N)[2:])
+    # print(binN, type(binN))
+    dictN = {}
+    for index in range(len(binN) - 1):
+        if index == len(binN) - 1:
+            return
+            # print(index, binN[index])
+        else:
+            if binN[index] == "1":
+                dictN[index] = []
+                for i in range(index + 1):
+                    if binN[i] == "0":
+                        dictN[index].append("0")
+    # print(dictN)
+    length = 0
+    for value in dictN.values():
+        if len(value) > length:
+            length = len(value)
+    return length
+
+
+
