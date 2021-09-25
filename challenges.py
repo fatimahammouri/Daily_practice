5d2456d0a839949a04fbe6f877a26983958632e0
Fatima <fatima.hammouri@gmail.com>
2021-09-25 09:34:44 +0000

    jewels&stones challenge

--- challenges.py
+++ challenges.py
@@ -438,4 +438,12 @@ def numIdenticalPairs(nums: List[int]) -> int:
             if nums[i] == nums[j]:
                 results.append((i,j))
     return len(results)
-                    
+                
+
+"""You're given strings jewels representing the types of stones that are jewels,
+and stones representing the stones you have. Each character in stones
+is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive,
+so "a" is considered a different type of stone from "A"""
+
