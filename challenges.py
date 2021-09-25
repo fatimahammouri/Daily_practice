1b2bc5cd1738f1cc6e3611f15c9d3b60be72b90a
Fatima <fatima.hammouri@gmail.com>
2021-09-25 10:19:15 +0000

    lessThanCurrent challenge

--- challenges.py
+++ challenges.py
@@ -493,3 +493,10 @@ class Solution:
                 counter += 1
                 
         return counter
+
+
+""" Given the array nums, for each nums[i] find out how many numbers
+in the array are smaller than it. That is, for each nums[i] you have to
+count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array."""
