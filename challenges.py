f7872d4bc5b80f946843e95c6cf5379a993dad5e
Fatima <fatima.hammouri@gmail.com>
2021-09-25 10:20:06 +0000

    lessThanCurrent possible Solution

--- challenges.py
+++ challenges.py
@@ -500,3 +500,29 @@ in the array are smaller than it. That is, for each nums[i] you have to
 count the number of valid j's such that j != i and nums[j] < nums[i].
 
 Return the answer in an array."""
+
+# inputs: array of integers
+# output: array of integers(counters for the number of integers less than the current integer)
+
+
+# declare results array
+# step1: iterate over the nums array _i
+# step2: declare a counter 
+# step3: iterate over the rest of the nums _j
+# step4: if nums[j] < nums[i]
+#        increase the counter
+# push the counter to the results
+
+
+def smallerNumbersThanCurrent(nums: List[int]) -> List[int]:
+    results = []
+    for i in range(len(nums)):
+        
+        counterForCurrent = 0
+        
+        for j in range(len(nums)):
+            if nums[j] < nums[i]:
+                counterForCurrent += 1
+        results.append(counterForCurrent)
+    return results
+                
