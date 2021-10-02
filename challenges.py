314301e47d64439e3721a08a68b4bed5c366c84b
Fatima <fatima.hammouri@gmail.com>
2021-10-02 09:20:49 +0000

    highFive challenge

--- challenges.py
+++ challenges.py
@@ -525,4 +525,15 @@ def smallerNumbersThanCurrent(nums: List[int]) -> List[int]:
                 counterForCurrent += 1
         results.append(counterForCurrent)
     return results
-                
+
+
+"""Given a list of the scores of different students, items,
+where items[i] = [IDi, scorei] represents one score from a student with IDi,
+calculate each student's top five average.
+
+Return the answer as an array of pairs result, where result[j] = [IDj, topFiveAveragej]
+represents the student with IDj and their top five average.
+Sort result by IDj in increasing order.
+
+A student's top five average is calculated by taking the sum of their top five scores
+and dividing it by 5 using integer division."""
